19c9c3b072675a106a34d15a79d6bf5bc515dd92
Octo System <50835071+OcotoSystem@users.noreply.github.com>
2019-10-09 22:57:34 +0200

    Update PLankton.ino

--- PLankton.ino
+++ PLankton.ino
@@ -78,7 +78,7 @@ byte mydata[2]; // size is 2
 static osjob_t sendjob;
 // Schedule TX every this many seconds (might become longer due to duty
 // cycle limitations).
-const unsigned TX_INTERVAL = 15;
+const unsigned TX_INTERVAL = 60;
 // Pin mapping
 const lmic_pinmap lmic_pins = {
   .nss = 10,
